05f09f4262b38720d30c5108906d15caec36ed1d
1998zxn <961943547@qq.com>
2020-03-13 10:21:30 -0700

    zxn

--- Lab1/src/Sudoku/sudoku.h
+++ Lab1/src/Sudoku/sudoku.h
@@ -5,21 +5,19 @@ const bool DEBUG_MODE = false;
 enum { ROW=9, COL=9, N = 81, NEIGHBOR = 20 };
 const int NUM = 9;
 
+
 extern int neighbors[N][NEIGHBOR];
-extern int board[N];
-extern int spaces[N];
-extern int nspaces;
+extern int board[30][N];
+extern int spaces[30][N];
+extern int nspaces[30];
 extern int (*chess)[COL];
 
 void init_neighbors();
-void input(const char in[N]);
+void input(const int i, const char in[N]);
 void init_cache();
 
-bool available(int guess, int cell);
+bool available(int i, int guess, int cell);
 
-bool solve_sudoku_basic(int which_space);
-bool solve_sudoku_min_arity(int which_space);
-bool solve_sudoku_min_arity_cache(int which_space);
-bool solve_sudoku_dancing_links(int unused);
-bool solved();
+bool solve_sudoku_basic(int i, int which_space);
+bool solved(int i);
 #endif
